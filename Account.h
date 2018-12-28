7639b7b4d0a4cb4ba14af9619845a4e377a80b5d
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-28 18:09:35 +0800

    Update Account.h

--- Account.h
+++ Account.h
@@ -1,18 +1,74 @@
-#ifndef Account_HEADER
-#define Account_HEADER 
+/**  Account class in a C++ code.
+*
+* #include "Account.h" <BR>
+* -llib
+*
+*/
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
 
-#include <iostream> 
+// SYSTEM INCLUDES
+#include<iostream>
 
-using namespace std; 
 
-class Account { 
-	int AccountID; 
-	float value;
-public: 
-	Account(float, int); 
-	void Deposit(float); 
-	void Withdraw(float); 
-	void GetBalance(); 
-}; 
+// Account class definition
+class Account {
+public:
+// LIFECYCLE
+	/** Default + Overloaded constructor.
+	*/
+	Account(int = 0, float = 0.0);
 
-#endif
+	// Use compiler-generated copy constructor, assignment, and destructor.
+	// Account(const Account&);
+	// Account& operator=(const Account&);
+	// ~Account();
+
+// OPERATIONS
+	/** function that deposits amount.
+	*
+	* @param aAmount The amount to be deposited.
+	*
+	* @return void
+	*/
+	void Deposit(float aAmount = 0.0);
+
+	/** function that withdraws amount.
+	*
+	* @param aAmount The amount to be withdrawn.
+	*
+	* @return void
+	*/
+	void Withdraw(float aAmount = 0);
+
+	/** function that prints balance.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void PrintBalance()const;
+
+// ACCESS
+	// setters
+	void SetAccountID(int = 0);
+	void SetValue(float = 0.0);
+	void SetAccount(int = 0, float = 0.0);
+	/**
+	# @overload void SetAccount(const Account&);
+	*/
+	void SetAccount(const Account&);
+	
+	// getters
+	int GetAccountID()const;
+	float GetValue()const;
+	const Account& GetAccount()const;
+
+private:
+// DATA MEMBERS
+	int mAccountID;
+	float mValue;
+};
+// end class Account
+#endif  
+// _ACCOUNT_H_
