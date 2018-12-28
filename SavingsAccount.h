cc33007de71fb9618e7f7f4245c66fffbf0746e6
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-28 18:11:46 +0800

    Update SavingsAccount.h

--- SavingsAccount.h
+++ SavingsAccount.h
@@ -1,7 +1,40 @@
-#include "Account.h" 
+/**  SavingsAccount class inherited from Account class.
+*
+* #include "SavingsAccount.h" <BR>
+* -llib
+*
+*/
+#ifndef SAVINGS_ACCOUNT_H
+#define SAVINGS_ACCOUNT_H
 
-class SavingsAccount :public Account { 
-public: 
-	SavingsAccount(float =0.0, int =0); 
-	void Withdraw(float); 
+// SYSTEM INCLUDES
+#include<iostream>
+
+// LOCAL INCLUDES
+#include "Account.h"
+
+// SavingsAccount class definition
+class SavingsAccount : public Account {
+public:
+// LIFECYCLE
+	/** Default + Overloaded constructor.
+	*/
+	SavingsAccount(int = 0, float = 0.0);
+
+	// Use compiler-generated copy constructor, assignment, and destructor.
+	// SavingsAccount(const SavingsAccount&);
+	// SavingsAccount& operator=(const SavingsAccount&);
+	// ~SavingsAccount();
+
+// OPERATIONS
+	/** Overriding function that withdraws amount from SavingsAccount.
+	*
+	* @param aAmount The amount to be withdrawn.
+	*
+	* @return void
+	*/
+	void Withdraw(float aAmount = 0);
 };
+// end class SavingsAccount
+#endif  
+// _SAVINGS_ACCOUNT_H_
