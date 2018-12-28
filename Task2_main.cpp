b625164a95653aecd51b1159e318535718eb1a2d
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-28 18:09:12 +0800

    Update Task2_main.cpp

--- Task2_main.cpp
+++ Task2_main.cpp
@@ -1,15 +1,33 @@
-#include"SavingsAccount.h" 
-#include "CurrentAccount.h" 
-#include "Bank.h" 
+/**
+* @file Task2_main.cpp
+*
+* @brief This code implements Banking system.
+* Current & Savings Account are inherited from Account class.
+* Bank has aggregation with Account class.
+*
+* @author Saif Ullah Ijaz
+*
+*/
 
-void main() { 
-	CurrentAccount sAccount1; 
-	sAccount1.Deposit((float)23.45); 
-	sAccount1.GetBalance(); 
-	sAccount1.Withdraw((float)102.8); 
-	
-	Bank b1("UBL", 0360, &sAccount1); 
-	b1.ShowBalance(); 
-	
-	system("Pause"); 
+// SYSTEM INCLUDES
+#include <iostream> 
+using namespace std;
+#include "Bank.h" 
+#include "CurrentAccount.h"
+#include "SavingsAccount.h"
+using namespace std;
+
+// function main begins program execution
+void main()
+{
+	CurrentAccount sAccount1;
+	sAccount1.Deposit(23.45);
+	sAccount1.PrintBalance();
+	sAccount1.Withdraw(102.8);
+
+	Bank b1("UBL", 360, &sAccount1);
+	b1.ShowBalance();
+
+	system("pause");
 }
+// end main
